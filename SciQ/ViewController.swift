29b3bd7b7f28fb7b09837e8eb455d0ece3c7c329
William Cheswick <ches@bork.local>
2019-08-10 09:06:05 -0700

    Initial Commit

---
+++ SciQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SciQ
+//
+//  Created by William Cheswick on 8/10/19.
+//  Copyright © 2019 William Cheswick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
